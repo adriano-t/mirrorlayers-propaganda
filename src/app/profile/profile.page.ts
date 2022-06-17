168e07087e9e8c56187cc22fb03a12acff3e883c
Adriano Tumminelli <adriano.tummi@hotmail.it>
2022-06-17 21:30:52 +0200

    post & comments on the profile

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Post, Profile, PropagandaService } from '../services/propaganda.service';
+import { Comment, GetMode, Language, Post, Profile, PropagandaService, SortMode } from '../services/propaganda.service';
 
 @Component({
   selector: 'app-profile',
@@ -11,6 +11,8 @@ export class ProfilePage implements OnInit {
 
   profile: Profile;
   posts: Post[];
+  comments: Comment[];
+  tab = 0;
 
   genders = [
     "Male",
@@ -28,13 +30,54 @@ export class ProfilePage implements OnInit {
     this.propaganda.getProfile(userId).subscribe((result) => {
       if(result.success) {
         this.profile = result.profile;
+        this.loadPosts();
+      }
+    });
+
+  }
+
+  loadPosts() {
+    console.log(this.profile.id)
+    this.propaganda.getPosts(GetMode.Begin, 0, 0, 255, this.profile.id, false, Language.All, SortMode.Enigma).subscribe((result) => {
+      if (result.success) {
+        this.posts = result.posts;
+        console.log(this.posts);
       }
     });
   }
 
+  loadComments() {
+    this.propaganda.getComments(GetMode.Begin, 0, 0, this.profile.id).subscribe((result) => {
+      if (result.success) {
+        this.comments = result.comments;
+        console.log(this.comments);
+      }
+    });
+  }
+
+  onCommentDelete() {
+    console.log("deleted comment");
+    //todo refresh
+  }
+
   segmentChanged(event) {
     console.log(event.detail.value);
-    
+    switch(event.detail.value) {
+      case "posts":
+        this.tab = 0;
+        this.loadPosts();
+        break;
+        
+      case "comments":
+        this.tab = 1;
+        this.loadComments();
+        break;
+        
+      case "followed":
+        this.tab = 2;
+        //this.loadFollowed();
+        break;
+    }
   }
 
 }
